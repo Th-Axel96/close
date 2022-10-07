70d1a06a6b31a6c4091118de1d91fc3d4b3dd75e
Th-Axel96 <axelmoussart77@hotmail.com>
2022-10-07 16:16:16 +0200

    update

--- pages/home/axel.jsx
+++
@@ -1,9 +0,0 @@
-const Axel = () => {
-    return (
-        <>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate pariatur vitae quibusdam alias velit facilis placeat eius consequuntur assumenda quia magni suscipit porro, dolores quasi officiis fuga laudantium aliquam quaerat.</p>
-        </>
-    )
-}
-
-export default Axel; 
