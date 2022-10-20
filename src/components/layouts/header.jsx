816c9d7e77381dfbd966f4de261e42c1af1ef5dc
Th-Axel96 <axelmoussart77@hotmail.com>
2022-10-20 08:50:29 +0200

    for revert

--- src/components/layouts/header.jsx
+++ src/components/layouts/header.jsx
@@ -5,19 +5,33 @@ import { NavbarMobile } from "./navbarMobile";
 // icon
 import MenuIcon from "@mui/icons-material/Menu";
 
+// emple useState
+// export const Header = () => {
+//   // function show with useState
+//   const [show, setShow] = useState(false);
+//   const Toggle = () => {
+//     setShow(!show);
+//   };
+//   return (
+//     <header className="bg-img ">
+//       <NavbarDesktop />
+//       <button onClick={Toggle}>
+//         <MenuIcon className="text-4xl md:hidden text-gold" />
+//       </button>
+//       {show === true ? <NavbarMobile /> : ""}
+//     </header>
+//   );
+// };
+
+
+
 export const Header = () => {
-  // function show with useState
-  const [show, setShow] = useState(false);
-  const Toggle = () => {
-    setShow(!show);
-  };
+  
   return (
-    <header className="bg-img ">
+    <header>
+      
       <NavbarDesktop />
-      <button onClick={Toggle}>
-        <MenuIcon className="text-4xl md:hidden text-gold" />
-      </button>
-      {show === true ? <NavbarMobile /> : ""}
+     <NavbarMobile  /> 
     </header>
   );
 };
--- src/components/layouts/navbarDesktop.jsx
+++ src/components/layouts/navbarDesktop.jsx
@@ -1,37 +1,39 @@
 import Link from "next/link";
 let styles = {
-  flex: "flex flex-row justify-around items-center h-40",
-  navDesktop:
-    "bg-clip-text text-transparent bg-gradient-to-b from-gold to-white font-spartan text-3xl uppercase  ",
-  borderLink: "border border-amber-800 shadow-xl p-4 rounded-xl",
-  hFull: "h-full"
+  bgBrown: "bg-brown",
+  textGold:
+    "bg-clip-text text-transparent bg-gradient-to-b from-gold to-white font-spartan  text-3xl uppercase  ",
+  flex: "flex  justify-around items-center h-40 w-full",
+  borderLink: "border border-gold shadow-xl p-4 rounded-xl",
 };
 
 export const NavbarDesktop = () => {
   return (
-    <nav>
-      <ul className={styles.flex}>
-        <li >
-          <Link href="/">
-            <a className={`${styles.navDesktop} ${styles.borderLink} `}>Home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/">
-            <a className={`${styles.navDesktop}`}>Home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/">
-            <a className={`${styles.navDesktop}`}>Home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/">
-            <a className={`${styles.navDesktop}`}>Home</a>
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <div className={`${styles.bgBrown} ${styles.flex}`}>
+      <nav>
+        <ul className={`${styles.flex} `}>
+          <li>
+            <Link href="/">
+              <a className={`${styles.textGold} ${styles.borderLink} `}>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a className={`${styles.textGold}`}>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a className={`${styles.textGold}`}>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a className={`${styles.textGold}`}>Home</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
