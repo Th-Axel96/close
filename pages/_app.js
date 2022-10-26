4433b648859c45289fe573fc385e40ecbd7c2e20
Axel-96 <106514343+Th-Axel96@users.noreply.github.com>
2022-10-26 10:59:48 +0200

    Delete pages directory

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import "tailwindcss/tailwind.css";
-import "@src/assets/css/styles.css";
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
